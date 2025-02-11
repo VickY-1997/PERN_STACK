ad33a6ab442f6b68c785fe9cfe20b4b13f93f45e
VickY-1997 <vigneshkumar3497@gmail.com>
2025-02-11 14:20:02 +0530

    database updated

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import productRoute from './routes/product.route.js'
+import { sql } from './config/db.js'
 
 dotenv.config()
 const app = express()
@@ -16,6 +17,27 @@ app.use(morgan("dev"))
 
 app.use('/api/products', productRoute)
 
+const initDB = async () => {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS product(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10,2) NOT NULL,
+        create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        )`
+        console.log(`initDb connected successfully`)
+    } catch (error) {
+        console.log(`Error in initDb`)
+    }
+}
+
+
+
+
+initDB().then(() => {
 app.listen(PORT, () => {
     console.log(`The server is running on PORT ${PORT}`)
+})
 })
